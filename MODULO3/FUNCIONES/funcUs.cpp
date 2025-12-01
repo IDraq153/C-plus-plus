028fd697358b2e2452cb0a62e9107c841dc8cf3c
Dario <darioruamelendez@gmail.com>
2025-12-01 08:49:24 -0500

    modFun

---
+++ MODULO3/FUNCIONES/funcUs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+    double x = 3.9;
+    double y = 15;
+    double z;
+
+    //FUNCIONES MAS USADAS
+    /*z = std::max(x,y);
+    z = std::min(x,y);
+    z = std::pow(x,y);
+    z = std::sqrt(9);
+    z = std::round(x);
+    z = std::ceil(x);
+    z = std::floor(x);
+    z = std::abs(-15);
+    cout << z;*/
+
+    return 0;
+}
