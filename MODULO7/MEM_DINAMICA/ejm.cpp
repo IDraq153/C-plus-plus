0245327498682e832572109a0c9f398a44706a52
darua <darioruamelendez@gmail.com>
2026-07-23 18:55:56 -0500

    fix

--- MODULO7/MEM_DINAMICA/ejm.cpp
+++ MODULO7/MEM_DINAMICA/ejm.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using std::cout;
-using std::cin;
 using string_t = std::string;
 
 int main() {
-    // new asigna memoria
-    int *pNum = nullptr;
-    //Esto apunta a una direccion donde guardaremos un entero
-    pNum = new int;
-    *pNum = 25;
+  // new asigna memoria
+  int *pNum = nullptr;
+  // Esto apunta a una direccion donde guardaremos un entero
+  pNum = new int;
+  *pNum = 25;
 
-    cout << "Direccion:" << pNum << std::endl;
-    cout << "Valor:" << *pNum << std::endl;
+  cout << "Direccion:" << pNum << std::endl;
+  cout << "Valor:" << *pNum << std::endl;
 
-    //BUENAS PRACTICAS LIBERAR MEMORIA EN ESA DIRECCION
-    delete pNum;
-    return 0;
+  // BUENAS PRACTICAS LIBERAR MEMORIA EN ESA DIRECCION
+  delete pNum;
+  return 0;
 }
